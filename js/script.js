6ca10d74e296e7aa4e2b8451bc21d492f5675369
Anita Kay <99274242+anitak456@users.noreply.github.com>
2022-04-27 02:52:35 +0000

    index.html

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// Created by: Anita Kay
+// Created on: April 2022
+// This file contains the JS functions for index.html
+
+"use strict";
+/**
+ * This function calculates your age on Mars and Venus.
+ */
+function calculate () {
+  // get the user's name and age
+  let name = document.getElementById('name').value;
+  let age = parseFloat(document.getElementById('age').value);
+  // calculate the age on Mars and Venus
+  let mars = age / 1.88;
+  let venus = age / 0.615;
+  //display the age on Mars
+  document.getElementById('answers').innerHTML = name + " is " + mars.toFixed(0) + " years old on Mars, and " + venus.toFixed(0) + " years old on Venus.";
+}
